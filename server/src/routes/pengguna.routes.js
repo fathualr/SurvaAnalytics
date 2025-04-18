29d8f0feb922a5fd7dffd27ef33e208cb586ddd2
fathualr <fathualrazi@gmail.com>
2025-04-18 14:51:00 +0700

    feat: add validation middleware to pengguna schema

--- server/src/routes/pengguna.routes.js
+++ server/src/routes/pengguna.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import * as userController from '../controllers/pengguna.controller.js';
+import { validate } from '../middlewares/validate.js';
+import { createPenggunaSchema, updatePenggunaSchema } from '../validations/pengguna.validation.js';
 
 const router = express.Router();
 
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUser);
-router.post('/', userController.createUser);
-router.patch('/:id', userController.updateUser);
+router.post('/', validate(createPenggunaSchema), userController.createUser);
+router.patch('/:id', validate(updatePenggunaSchema), userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 export default router;
