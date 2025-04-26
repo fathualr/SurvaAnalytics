1e0121fb552e2afdaf878d819fd074faff57b03b
fathualr <fathualrazi@gmail.com>
2025-04-27 00:53:09 +0700

    feat: add validations for register services

--- server/src/routes/auth/register.routes.js
+++ server/src/routes/auth/register.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import * as registerController from '../../controllers/auth/register.controller.js';
+import { validate } from '../../middlewares/validate.js';
+import { emailRegisterValidation, verifyOTPValidation, completeAccountValidation } from '../../validations/auth.validation.js';
 
 const router = express.Router();
 
-router.post('/', registerController.emailRegister);
-router.post('/verify', registerController.verifyOTP);
-router.post('/account', registerController.completeAccount);
+router.post('/', validate(emailRegisterValidation), registerController.emailRegister);
+router.post('/verify', validate(verifyOTPValidation), registerController.verifyOTP);
+router.post('/account', validate(completeAccountValidation), registerController.completeAccount);
 
 export default router;
--- server/src/validations/auth.validation.js
+++ server/src/validations/auth.validation.js
@@ -4,3 +4,20 @@ export const loginValidation = Joi.object({
   email: Joi.string().email().max(255).required(),
   password: Joi.string().min(8).required(),
 });
+
+export const emailRegisterValidation = Joi.object({
+  email: Joi.string().email().max(255).required(),
+});
+
+export const verifyOTPValidation = Joi.object({
+  email: Joi.string().email().max(255).required(),
+  otp: Joi.string().length(6).required()
+});
+
+export const completeAccountValidation = Joi.object({
+  register_token: Joi.string().required(),
+  password: Joi.string().min(8).required(),
+  nama: Joi.string().required(),
+  profil_responden: Joi.object().optional(),
+  profil_klien: Joi.object().optional()
+});
