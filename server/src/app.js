19b8e0d4c06d18d4e7edf1334e1c1090f320f687
fathualr <fathualrazi@gmail.com>
2025-04-26 09:53:04 +0700

    feat: initialize JWT utilities and cookie parser

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import routes from './routes/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api', routes);
 
