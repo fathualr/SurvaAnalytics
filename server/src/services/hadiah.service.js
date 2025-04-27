255fb4dd6979bd130b853c57fa0726953b986b26
Satria3312301092 <muhammadsatriaadjisandi@gmail.com>
2025-04-27 19:34:04 +0700

    feat: add feature data management hadiah

---
+++ server/src/services/hadiah.service.js
@@ -0,0 +1,57 @@
+import db from'../models/index.js';
+const { Hadiah, sequelize } = db;
+
+export const index = async () => {
+    return await Hadiah.findAll({
+    });
+};
+
+export const create = async (hadiahData) => {
+    const transaction = await sequelize.transaction();
+
+    try {
+        const hadiah = await Hadiah.create(hadiahData, { transaction });
+        await transaction.commit();
+        return hadiah;
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+};
+
+export const show = async (hadiahId) => {
+    const hadiah = await Hadiah.findByPk(hadiahId);
+    if (!hadiah) throw new Error('Hadiah not found');
+    return hadiah;
+};
+
+export const update = async (hadiahId, updateData) => {
+    const transaction = await sequelize.transaction();
+
+    try {
+        const hadiah = await Hadiah.findByPk(hadiahId, { transaction });
+        if (!hadiah) throw new Error('Hadiah not found');
+
+        await hadiah.update(updateData, { transaction });
+        await transaction.commit();
+        return hadiah;
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+};
+
+export const destroy = async (hadiahId) => {
+    const transaction = await sequelize.transaction();
+
+    try {
+        const hadiah = await Hadiah.findByPk(hadiahId, { transaction });
+        if (!hadiah) throw new Error('Hadiah not found');
+
+        await hadiah.destroy({ transaction });
+        await transaction.commit();
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
+};
---
+++ server/src/validations/hadiah.validation.js
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+export const createHadiahSchema = Joi.object({
+    deskripsi: Joi.string().optional(),
+    stok: Joi.number().integer().min(0).required(),
+    harga_poin: Joi.number().integer().min(0).required()
+});
+
+export const updateHadiahSchema = Joi.object({
+    deskripsi: Joi.string().optional(),
+    stok: Joi.number().integer().min(0).optional(),
+    harga_poin: Joi.number().integer().min(0).optional()
+});
