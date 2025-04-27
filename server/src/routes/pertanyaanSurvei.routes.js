7094287d967cd0d7fcda15f4c40f0d734048d5da
fathualr <fathualrazi@gmail.com>
2025-04-27 23:01:58 +0700

    feat: add validations to pertanyaan survei CRUD operations

--- server/src/routes/pertanyaanSurvei.routes.js
+++ server/src/routes/pertanyaanSurvei.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import * as pertanyaanSurveiController from '../controllers/pertanyaanSurvei.controller.js';
+import { validate } from '../middlewares/validate.js';
+import { createPertanyaanSurveiSchema, updatePertanyaanSurveiSchema } from '../validations/pertanyaanSurvei.validation.js';
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', pertanyaanSurveiController.getpertanyaanSurveis);
 router.get('/:id', pertanyaanSurveiController.getPertanyaanSurvei);
-router.post('/', pertanyaanSurveiController.createPertanyaanSurvei);
-router.patch('/:id', pertanyaanSurveiController.updatePertanyaanSurvei);
+router.post('/', validate(createPertanyaanSurveiSchema), pertanyaanSurveiController.createPertanyaanSurvei);
+router.patch('/:id', validate(updatePertanyaanSurveiSchema), pertanyaanSurveiController.updatePertanyaanSurvei);
 router.delete('/:id', pertanyaanSurveiController.deletePertanyaanSurvei);
 
 export default router;
