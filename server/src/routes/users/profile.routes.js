f01d8a57ccdab00d5bdd951e1c0df3e37eceb4a4
fathualr <fathualrazi@gmail.com>
2025-05-04 16:50:28 +0700

    feat: add validations to update profile

--- server/src/routes/users/profile.routes.js
+++ server/src/routes/users/profile.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import * as profileController from '../../controllers/users/profile.controller.js';
 import { authenticate, authorize } from '../../middlewares/auth.js';
+import { validate } from '../../middlewares/validate.js';
+import { updateProfileSchema } from '../../validations/users/profile.validation.js';
 
 const router = express.Router();
 
 router.get('/', authenticate, authorize('umum'), profileController.getProfile);
-router.patch('/', authenticate, authorize('umum'), profileController.updateProfile);
+router.patch('/', authenticate, authorize('umum'), validate(updateProfileSchema), profileController.updateProfile);
 
 export default router;
