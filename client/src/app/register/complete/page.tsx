b84a431d419c6e4d1d6da7bb83b351a0d4404b8b
Gutserk <ardanasmirza@gmail.com>
2025-05-17 06:50:46 +0700

    fix: change route url and add a few input

--- client/src/app/register/complete/page.tsx
+++ client/src/app/register/complete/page.tsx
@@ -14,25 +14,22 @@ import { Button } from "@/components/ui/button"
 
 export default function Register() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Card className="p-8 flex flex-col gap-2 bg-[url('/images/background-form.png')] bg-cover w-[750px] h-[500px] rounded-none rounded-bl-[70px] rounded-tr-[70px] ">
+    <div className="flex items-center justify-center min-h-screen py-8">
+      <Card className="p-8 flex flex-col gap-2 bg-[url('/images/background-form.png')] bg-cover w-[750px] h-[730px] rounded-none rounded-bl-[70px] rounded-tr-[70px] ">
         <CardHeader className="items-center justify-center">
-          <CardTitle className="font-semibold text-3xl text-white ">Sign up</CardTitle>
+          <CardTitle className="font-semibold text-3xl text-white ">Profile Details</CardTitle>
         </CardHeader>
 
-        <CardContent className="pt-4">
+        <CardContent className="">
           <div className="flex flex-wrap gap-2">
-            <div className="flex w-full gap-2">
-                <div className="flex-1">
+            <div className="w-full">
                 <label htmlFor="nama" className="text-md text-white font-semibold">Nama</label>
                 <Input className="border-none bg-white rounded-sm h-10 text-black w-full" id="nama" type="text" />
-                </div>
-                <div className="flex-1">
-                <label htmlFor="tanggal" className="text-md text-white font-semibold">Tanggal Lahir</label>
-                <input className="w-full pl-2 border-none bg-white rounded-sm h-10 text-black" id="tanggal" type="date" />
-                </div>
             </div>
-
+            <div className="w-full">
+                <label htmlFor="tanggal" className="text-md text-white font-semibold">Tanggal Lahir</label>
+                <Input className="w-full pl-2 border-none bg-white rounded-sm h-10 text-black" id="tanggal" type="date" />
+            </div>
             <div className="w-full">
                 <label htmlFor="email" className="text-md text-white font-semibold">Email</label>
                 <Input className="border-none bg-white rounded-sm w-full h-10 text-black" id="email" type="email" />
@@ -47,6 +44,26 @@ export default function Register() {
                 <label htmlFor="confpassword" className="text-md text-white font-semibold">Confirm Password</label>
                 <Input className="border-none bg-white rounded-sm w-full h-10 text-black" id="confpassword" type="password" />
             </div>
+
+            <div className="w-full">
+              <label htmlFor="jenis_kelamin" className="text-md text-white font-semibold">Jenis Kelamin</label>
+              <select id="jenis_kelamin" name="jenis_kelamin" className="border-none bg-white rounded-sm w-full h-10 text-black px-2" >
+                <option value="" aria-readonly>Pilih Jenis Kelamin </option>
+                <option value="laki-laki">Laki-laki</option>
+                <option value="perempuan">Perempuan</option>
+              </select>
+            </div>
+
+            <div className="w-full">
+                <label htmlFor="domisili" className="text-md text-white font-semibold">Asal Domisili</label>
+                <Input className="border-none bg-white rounded-sm w-full h-10 text-black" id="domisili" type="password" />
+            </div>
+
+            <div className="w-full">
+                <label htmlFor="status" className="text-md text-white font-semibold">Status Anda </label>
+                <Input className="border-none bg-white rounded-sm w-full h-10 text-black" id="status" type="password" />
+            </div>
+            
             <div className="w-full flex justify-end pt-4">
               <Button className="w-32 rounded-sm text-md bg-[#FFBF68] text-white hover:bg-gray-200">
                 Buat
