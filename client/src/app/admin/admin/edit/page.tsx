0d4ee1cfc0bec6a4947f413375c4aa14182cf446
Gutserk <ardanasmirza@gmail.com>
2025-06-01 20:16:01 +0700

    feat: add manage-admin page, detail page and edit page in manage admin page

---
+++ client/src/app/admin/admin/edit/page.tsx
@@ -0,0 +1,69 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+export default function EditPage() {
+  return (
+    <div className="pl-2 bg-white">
+      <h2 className="text-2xl font-semibold">Detail Admin</h2>
+      <form className="w-full bg-[#F2F9FF] border border-[#3E82CD] p-4 rounded-xl mt-6 shadow-sm">
+        <div className="divide-y divide-[#D0E3F6]">
+          
+          <div className="grid grid-cols-[auto_1fr] gap-x-4 py-2 items-center">
+            <label htmlFor="nama" className="text-base font-semibold">Nama:</label>
+            <Input id="nama" type="text" placeholder="Nama" defaultValue="Abdul micro" className="text-base w-48 justify-self-end text-right" />
+          </div>
+
+          <div className="grid grid-cols-[auto_1fr] gap-x-4 py-2 items-center">
+            <label htmlFor="email" className="text-base font-semibold">Email:</label>
+            <Input id="email" type="email" placeholder="Email" defaultValue="abdul@gmail.com" className="text-base w-48 justify-self-end text-right" />
+          </div>
+
+          <div className="grid grid-cols-[auto_1fr] gap-x-4 py-2 items-center">
+            <label htmlFor="kontak" className="text-base font-semibold">Kontak:</label>
+            <Input id="kontak" type="text" defaultValue="08123456789101" className="text-base w-32 justify-self-end text-center border-none" />
+          </div>
+        </div>
+
+        <div className="flex justify-end mt-4">
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button className="bg-[#FFBF68] hover:opacity-60 hover:bg-[#FFBF68]" type="button" variant="default">Simpan</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Konfirmasi Simpan</AlertDialogTitle>
+              </AlertDialogHeader >
+              <p>Apakah kamu yakin ingin menyimpan perubahan?</p>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Batal</AlertDialogCancel>
+                <AlertDialogAction asChild>
+                  <Button type="submit" className="bg-[#FFBF68] hover:opacity-60 hover:bg-[#FFBF68] text-white px-4 py-2 rounded-md ">
+                    Ya, Simpan
+                  </Button>
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+      </form>
+    </div>
+  )
+}
