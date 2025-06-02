cfd48569149e4e6031d6391e3fefb5988eed9724
fathualr <fathualrazi@gmail.com>
2025-06-02 10:01:22 +0700

    refactor: movie useCountdown to global hook

--- client/src/features/auth/components/verify-otp-form.tsx
+++ client/src/features/auth/components/verify-otp-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { useCountdown } from '../hooks/useCountdown'
+import { useCountdown } from '@/hooks/useCountdown'
 import { formatTime } from '@/utils/format'
 import {
   InputOTP,
